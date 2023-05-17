e4f210aa7af99b2e0a6d46337aa3aaaf5e6f107f
Lulit25 <lulitm5489@gmail.com>
2023-05-16 21:56:34 -0500

    updates readme file

--- Frontend/SampleProject/src/routes/routes.tsx
+++ Frontend/SampleProject/src/routes/routes.tsx
@@ -10,7 +10,6 @@ function ClientRoutes() {
   return (
     <Router>
       {/* <React.Suspense fallback={<Loader type="Oval" color="#00BFFF" height={80} width={80} />}> */}
-
       <Routes>
         <Route path="/" element={<ClientListing />} />
         <Route path="/client/:id" element={<ClientDetail />} />
